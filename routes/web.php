73bf54c7be8b03a53c2071206a21512630d3ba00
nabellaag <nabellaayug@students.amikom.ac.id>
2023-07-28 05:30:58 +0700

    build

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//mengatur rute untuk halaman utama
+Route::get('/', [FrontController::class, 'index']);
+
+//mengatur rute untuk halaman dashboard
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/posts', PostController::class);//lebih cepat daripada pakai path
+});
+
+require __DIR__.'/auth.php';
